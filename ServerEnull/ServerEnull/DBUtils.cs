98956c84b0b90a8dbb38ede760afb5ae94f98ba1
artykisa <48124997+artykisa@users.noreply.github.com>
2020-03-25 20:16:34 +0300

    Add server for login form

---
+++ ServerEnull/ServerEnull/DBUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+
+namespace ServerEnull
+{
+    class DBUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string datasource = @"192.168.205.135\SQLEXPRESS";
+
+            string database = "simplehr";
+            string username = "sa";
+            string password = "1234";
+
+            return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
+        }
+    }
+}
